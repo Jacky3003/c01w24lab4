cd3e6bcb413f91b6f3d970f10bf1cb4c8d80d8ae
Anthony qin <qin2500@gmail.com>
2024-02-05 15:57:24 -0500

    Finished person4 stuff

--- backend/server.js
+++ backend/server.js
@@ -136,3 +136,32 @@ app.delete("/deleteAllNotes", express.json(), async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 })
+
+app.post("/postNote", express.json(), async (req, res) => {
+  try {
+    // Basic body request check
+    const { title, content } = req.body;
+    const createdAt = new Date();
+
+    if (!title || !content) {
+      return res
+        .status(400)
+        .json({ error: "Title and content are both required." });
+    }
+
+    // Send note to database
+    const collection = db.collection(COLLECTIONS.notes);
+    const result = await collection.insertOne({
+      title,
+      content,
+      createdAt
+    });
+    res.json({
+      response: "Note added succesfully.",
+      insertedId: result.insertedId,
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
